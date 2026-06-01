8baa771fe52d47ae9f36096650bbd045ecd923c6
cwtechshiz <62831670+cwtechshiz@users.noreply.github.com>
2026-06-01 19:01:42 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Dynamically target the current user's home directory
 PLUGIN_DIR="$HOME/homebrew/plugins/nordvpn-decky"
-DOWNLOAD_URL="https://github.com/YOUR_USERNAME/nordvpn-decky/releases/latest/download/nordvpn-decky.zip"
+DOWNLOAD_URL="https://github.com/cwtechshiz/nordvpn-decky/releases/latest/download/nordvpn-decky.zip"
 
 echo "Downloading latest NordVPN Decky Plugin for profile: $USER..."
 mkdir -p /tmp/nordvpn-decky-install
